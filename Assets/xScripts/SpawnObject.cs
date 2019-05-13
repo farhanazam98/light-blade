9e5e1949392143b31435cdd9e1af203535b6a952
Farhan Azam <farhanazam98@yahoo.com>
2019-05-12 18:41:56 -0700

    Initial Commit

---
+++ Assets/xScripts/SpawnObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+
+	public GameObject cubePrefab;
+
+	public Vector3 center;
+	public Vector3 size;
+
+    private float startTime;
+    private float EPSILON = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(System.Math.Abs(Time.time - startTime - 1) < EPSILON)
+        {
+
+            SpawnCube();
+            startTime = Time.time;
+        }
+	}
+
+	public void SpawnCube()
+	{
+		Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x/2), Random.Range(-size.y / 2, size.y/2), 
+			Random.Range(-size.z / 2, size.z/2));
+		Instantiate (cubePrefab, pos, Quaternion.identity);
+	}
+		
+	void onDrawGizmosSelected()
+	{
+		Gizmos.color = new Color (1, 0, 0);
+		Gizmos.DrawCube (center, size);
+	}
+
+}
---
+++ Assets/xScripts/cubeScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeScript : MonoBehaviour {
+
+	public float speed = 1.0f;
+	public Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float step =  speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target, step);
+
+		if (Vector3.Distance(transform.position, target) < 0.001f)
+		{
+            // remove object
+            Destroy(gameObject);
+		}
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("lightblade"))
+        {
+            Destroy(gameObject);
+
+        }
+    }
+}
